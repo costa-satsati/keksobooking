98bca8cdb160f694c1aa213ccb11c79ecec25e4b
Konstantin Sattsaev <Konstantin.Sattsaev@health.qld.gov.au>
2020-02-28 08:09:32 +1000

    genrate mock listings

--- js/main.js
+++ js/main.js
@@ -1,23 +1,65 @@
 'use strict';
 
+var similarListingElement = document.querySelector('.map__pins');
+var mapElement = document.querySelector('.map');
+mapElement.classList.remove('map--faded');
+var LISTING_TITLES = ['Дамблдор', 'Волдеморт', 'Доктор Стрендж', 'Гарри Поттер', 'Дамблдор', 'Волдеморт', 'Доктор Стрендж', 'Гарри Поттер'];
+var TIMES = ['12:00', '13:00', '14:00'];
+var pinTemplate = document.querySelector('#pin')
+  .content
+  .querySelector('.map__pin');
+
+var getRandomInt = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
 var generatesSimilarListings = function () {
-    var similarListings = [];
-    var listingObject = {
-        author: {},
-        offer: {},
-        location: {}
-    };
+  var similarListings = [];
 
-    for (var i = 1; i < 9; i++) {
-        var listing = Object.create(listingObject);
-        listing.author.avatar = 'img/avatars/user0' + i + '.png';
-        similarListings.push(listing);
-    }
+  for (var i = 0; i < 8; i++) {
+    similarListings.push({
+      author: {
+        avatar: 'img/avatars/user0' + (i + 1) + '.png'
+      },
+      offer: {
+        title: LISTING_TITLES[i],
+        address: '600, 350',
+        price: '12 000',
+        type: ['palace', 'flat', 'house' , 'bungalo'],
+        rooms: ['1','2','3','4'],
+        guests: ['1','2','3','4','5'],
+        photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+        checkin: TIMES[Math.floor(Math.random() * TIMES.length)],
+        checkout: TIMES[Math.floor(Math.random() * TIMES.length)],
+        features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']
+      },
+      location: {
+        x: getRandomInt(0, mapElement.offsetWidth),
+        y: getRandomInt(130, 630)
+      }
+    });
+  }
 
-    return similarListings;
+  return similarListings;
+};
+
+var renderListing = function (listing) {
+  var listingElement = pinTemplate.cloneNode(true);
+
+  listingElement.querySelector('img').src = listing.author.avatar;
+  listingElement.querySelector('img').alt = listing.offer.title;
+  listingElement.style.left = listing.location.x + 'px';
+  listingElement.style.top = listing.location.y + 'px';
+
+  return listingElement;
+};
+
+var fragment = document.createDocumentFragment();
+var mockListings = generatesSimilarListings();
+
+for (var i = 0; i < mockListings.length; i++) {
+  fragment.appendChild(renderListing(mockListings[i]));
 }
-
-console.log(generatesSimilarListings());
-
-var mapBlock = document.querySelector('.map');
-mapBlock.classList.remove('map--faded');
+similarListingElement.appendChild(fragment);
