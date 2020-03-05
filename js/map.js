215be62201ab582ee45b55f8b28c4ae9760926a9
Konstantin Sattsaev <Konstantin.Sattsaev@health.qld.gov.au>
2020-03-05 10:18:54 +1000

    rearange items

--- js/map.js
+++ js/map.js
@@ -7,6 +7,30 @@
     var adForm = document.querySelector('.ad-form');
     var adFormElements = adForm.querySelectorAll('fieldset');
 
+    var successHandler = function (listings) {
+        window.data.listingObjects = listings;
+
+        var fragment = document.createDocumentFragment();
+
+        for (var i = 0; i < window.data.listingObjects.length; i++) {
+            fragment.appendChild(window.pin.renderListing(window.data.listingObjects[i], i));
+        }
+        similarListingElement.appendChild(fragment);
+    };
+
+    var errorHandler = function (errorMessage) {
+        var node = document.createElement('div');
+        node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+        node.style.position = 'absolute';
+        node.style.left = 0;
+        node.style.right = 0;
+        node.style.fontSize = '30px';
+
+        node.textContent = errorMessage;
+        document.body.insertAdjacentElement('afterbegin', node);
+    };
+
+
     var mainPinClickHandler = function (evt) {
         var mapFadedElement = document.querySelector('.map--faded');
 
@@ -14,40 +38,19 @@
             (evt.button === 0 || evt.key === 'Enter')) {
             mapElement.classList.remove('map--faded');
             adForm.classList.remove('ad-form--disabled')
-           
+
+            // загрузить данные сервера
+            window.load(successHandler, errorHandler);
 
             // enable ad form elements
             for (var i = 0; i < adFormElements.length; i++) {
                 adFormElements[i].disabled = false;
             }
-                     
+
         }
     };
 
-    var successHandler = function (listings) {
-        window.data.listingObjects = listings;
 
-        var fragment = document.createDocumentFragment();         
-
-        for (var i = 0; i < window.data.listingObjects.length; i++) {
-            fragment.appendChild(window.pin.renderListing(window.data.listingObjects[i], i));
-        }
-        similarListingElement.appendChild(fragment);
-      };
-      
-      var errorHandler = function (errorMessage) {
-        var node = document.createElement('div');
-        node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
-        node.style.position = 'absolute';
-        node.style.left = 0;
-        node.style.right = 0;
-        node.style.fontSize = '30px';
-        
-        node.textContent = errorMessage; 
-        document.body.insertAdjacentElement('afterbegin', node);
-      };
-      
-      window.load(successHandler, errorHandler);
 
     pinMain.addEventListener('mousedown', mainPinClickHandler);
     pinMain.addEventListener('keydown', mainPinClickHandler);
