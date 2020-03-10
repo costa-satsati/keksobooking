f9280dac8702a1f6431306553bc6d8540ca7b10d
Konstantin Sattsaev <Konstantin.Sattsaev@health.qld.gov.au>
2020-03-10 12:06:36 +1000

    add form submit

--- js/form.js
+++ js/form.js
@@ -9,6 +9,10 @@
     var capacity = document.querySelector('#capacity');
     var avatarUpload = document.querySelector('#avatar');
     var avatarImage = document.querySelector('.ad-form-header__preview img');
+    var Code = {
+        OK: 200,
+        ERROR: 400
+    };
 
     for (var i = 0; i < adFormElements.length; i++) {
         adFormElements[i].disabled = true;
@@ -47,6 +51,22 @@
         if (avatarUpload.files && avatarUpload.files[0]) {
             avatarImage.src = URL.createObjectURL(avatarUpload.files[0]);
         }
+    });
+
+    adForm.addEventListener('submit', function (evt) {
+        window.upload(new FormData(adForm), function (xhr) {
+            switch (xhr.status) {
+                case Code.OK:
+                    console.log(xhr.status);
+                    break;
+                case Code.ERROR:
+                    console.log(xhr.status);
+                    break;
+                default:
+                    console.log('Unknown response!')
+            }
+        });
+        evt.preventDefault();
     });
 
 
