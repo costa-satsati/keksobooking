da8b0237cb3d9cef7516ac0d7612006fe8e8bdcc
Konstantin Sattsaev <Konstantin.Sattsaev@health.qld.gov.au>
2020-03-12 11:56:01 +1000

    set correct coordinates

--- js/pin.js
+++ js/pin.js
@@ -1,6 +1,9 @@
 (function () {
     'use strict';
 
+    var PIN_WIDTH = 50;
+    var PIN_HEIGHT = 70;
+
     var pinTemplate = document.querySelector('#pin')
         .content
         .querySelector('.map__pin');
@@ -10,14 +13,14 @@
 
         listingElement.querySelector('img').src = listing.author.avatar;
         listingElement.querySelector('img').alt = listing.offer.title;
-        listingElement.style.left = listing.location.x + 'px';
-        listingElement.style.top = listing.location.y + 'px';
+        listingElement.style.left = (listing.location.x - PIN_WIDTH / 2) + 'px';
+        listingElement.style.top = (listing.location.y - PIN_HEIGHT) + 'px';
 
         listingElement.dataset.indexNumber = listing.id;
         listingElement.addEventListener('click', window.card.displayPopup);
 
         return listingElement;
-    };   
+    };
 
     window.pin = {
         renderListing: renderListing
