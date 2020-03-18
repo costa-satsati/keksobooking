4dda11bb78c3df8a76fee906056a5d36dc0e67ff
Konstantin Sattsaev <Konstantin.Sattsaev@health.qld.gov.au>
2020-03-19 08:11:04 +1000

    merge projects

--- js/ajax.js
+++ js/ajax.js
@@ -1,12 +1,25 @@
 'use strict';
 
 (function () {
-  var URL = 'https://js.dump.academy/keksobooking/data';
-  
-  window.load = function (onSuccess, onError) {
+  var URL_GET = 'https://js.dump.academy/keksobooking/data';
+  var URL_POST = 'https://js.dump.academy/keksobooking';
+
+  var upload = function (data, onSuccess) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-    
+
+    xhr.addEventListener('load', function () {
+      onSuccess(xhr);
+    });
+
+    xhr.open('POST', URL_POST);
+    xhr.send(data);
+  };
+
+  var load = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
         onSuccess(xhr.response);
@@ -20,11 +33,16 @@
     xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
-    
+
     xhr.timeout = 10000; // 10s
-    
-    xhr.open('GET', URL);
+
+    xhr.open('GET', URL_GET);
     xhr.send();
+  };
+
+  window.ajax = {
+    load: load,
+    upload: upload
   };
 
 })();
